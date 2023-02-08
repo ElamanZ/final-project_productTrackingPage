04c587a778e953fd527032752848632c83703ff0
elamanz <z.elaman699@gmail.com>
2023-02-09 03:40:24 +0600

    productTrackingPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import MainPage from "./pages/mainPage/MainPage";
 import Layout from "./components/layout/Layout";
+import Product_trackingPage from "./pages/product_trackingPage/ProductTrackingPage";
 
 function App() {
   return (
     <div className="App">
-        <Layout>
-            <MainPage/>
-        </Layout>
+        {/*<Layout>*/}
+            {/*<MainPage/>*/}
+            <Product_trackingPage/>
+        {/*</Layout>*/}
     </div>
   );
 }
